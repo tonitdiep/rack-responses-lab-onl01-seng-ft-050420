b7ef00cabb9673a5e0d941f0ef2c1fe6aeb65628
Toni  Diep <adiep123@gmail.com>
2020-08-23 02:47:24 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application 
+  
+  def call(env)
+  #   resp = Rack::Response.new
+  #   if time is before noon 
+  #     resp.write "Good Morning!"
+  #   else
+  #     resp.write "Good Afternoon!"
+  # end
+
+  # resp.finish 
+end
